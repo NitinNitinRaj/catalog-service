9acf152da5f34cc20da61541bca73483f4337161
NitinNitinRaj <bnitinraj@gmail.com>
2023-05-01 10:54:59 +0530

    refactoring schema for JDBC auditing

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,13 @@
 DROP TABLE IF EXISTS book;
-CREATE TABLE book(
-    id      BIGSERIAL PRIMARY KEY NOT NULL,
-    author  VARCHAR(255) NOT NULL,
-    isbn    VARCHAR(255) UNIQUE NOT NULL,
-    price   FLOAT8 NOT NULL,
-    title   VARCHAR(255) NOT NULL,
-    version INTEGER NOT NULL
-);
+
+CREATE TABLE
+    book(
+        id BIGSERIAL PRIMARY KEY NOT NULL,
+        author VARCHAR(255) NOT NULL,
+        isbn VARCHAR(255) UNIQUE NOT NULL,
+        price FLOAT8 NOT NULL,
+        title VARCHAR(255) NOT NULL,
+        createdDate TIMESTAMP NOT NULL,
+        lastModifiedDate TIMESTAMP NOT NULL,
+        version INTEGER NOT NULL
+    );
